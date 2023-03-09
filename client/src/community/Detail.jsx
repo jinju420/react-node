1186ee6211ddc3481b59a3b1e0a10574a00dd363
jinju420 <ljjshlove@nate.com>
2023-03-09 12:22:21 +0900

    로그인시에만 상제페이지 수정, 삭제버튼 출력

--- client/src/community/Detail.jsx
+++ client/src/community/Detail.jsx
@@ -3,6 +3,7 @@ import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 
 const DetailWrap = styled.div`
 	width: 100%;
@@ -18,6 +19,7 @@ const BtnSet = styled.nav`
 `;
 
 function Detail() {
+	const user = useSelector((store) => store.user);
 	const params = useParams();
 	const navigate = useNavigate();
 	const [Detail, setDetail] = useState(null);
@@ -58,13 +60,14 @@ function Detail() {
 						<h2>{Detail?.title}</h2>
 						<p>{Detail?.content}</p>
 					</DetailWrap>
-
-					<BtnSet>
-						<button>
-							<Link to={`/edit/${params.num}`}>EDIT</Link>
-						</button>
-						<button onClick={handleDelete}>DELETE</button>
-					</BtnSet>
+					{user.uid !== '' && (
+						<BtnSet>
+							<button>
+								<Link to={`/edit/${params.num}`}>EDIT</Link>
+							</button>
+							<button onClick={handleDelete}>DELETE</button>
+						</BtnSet>
+					)}
 				</>
 			}
 		</Layout>
